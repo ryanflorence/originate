2ee890060e1d15ea694dd4fec78d4a4b602c28f6
Ryan Florence <rpflorence@gmail.com>
2013-12-01 19:32:22 -0700

    rearrange

--- index.js
+++ index.js
@@ -7,61 +7,62 @@ var red = color.red;
 var prompt = require('cli-prompt');
 
 module.exports = function() {
+  maybeLogHelpOrVersion();
+  validateArgs();
+  run();
+};
 
-  if (process.argv[2]) {
-    if (process.argv[2].match(/(--help|-h)/)) {
-      logHelp();
-      process.exit();
-    }
-
-    if (process.argv[2].match(/(--version|-v)/)) {
-      console.log('v'+require('./package').version);
-      process.exit();
-    }
-  }
-
-  if (process.argv.length !== 4) {
-    logHelp();
-    process.exit();
-  }
-
+function run() {
   var origin = process.argv[2];
   var dest = process.argv[3];
-  var moduleName = 'loom-origin-'+origin;
+  var moduleName = 'originate-'+origin;
   var createdNodeModulesDir = false;
+  var loomCommand = '--path node_modules/'+moduleName+'/loom '+origin+' '+dest;
 
-  if (!fs.existsSync('node_modules')) {
-    createdNodeModulesDir = true;
-    log('making temporary node_modules directory');
-    mkdir('node_modules');
+  ensureNodeModules();
+  installModule();
+  validateInstall();
+  loom(loomCommand, afterLoom);
+
+  function afterLoom() {
+    cleanup();
+    log('new '+origin+' project is ready to go at: '+dest);
   }
 
-  log('installing '+moduleName);
-  exec('npm install '+moduleName);
-
-  var dir = 'node_modules/'+moduleName;
-
-  if (!fs.existsSync(dir)) {
-    log(red('could not install '+moduleName+' from npm, exiting\n'));
-    process.exit();
+  function ensureNodeModules() {
+    if (!fs.existsSync('node_modules')) {
+      createdNodeModulesDir = true;
+      log('making temporary node_modules directory');
+      mkdir('node_modules');
+    }
   }
 
-  loom('--path node_modules/'+moduleName+'/loom '+origin+' '+dest, function() {
+  function installModule() {
+    log('installing '+moduleName);
+    exec('npm install '+moduleName);
+  }
+
+  function validateInstall() {
+    var dir = 'node_modules/'+moduleName;
+    if (!fs.existsSync(dir)) {
+      log(red('could not install '+moduleName+' from npm, exiting\n'));
+      process.exit();
+    }
+  }
+
+  function cleanup() {
     log('removing node_modules/'+moduleName);
     rm('-rf', 'node_modules/'+moduleName);
     if (createdNodeModulesDir) {
       log('removing temporary node_modules directory');
       mkdir('-rf', 'node_modules');
     }
-    log('new '+origin+' project is ready to go at: '+dest);
-  });
-
+  }
 };
 
 function log(msg) {
   console.log('\n'+blue('originate:'), msg);
 }
-
 
 function logHelp() {
   console.log('\n  Usage:');
@@ -72,3 +73,24 @@ function logHelp() {
   console.log('\n    Publish projects to npm as "originate-<name>" to create\n    new origins; others can then use them immediately.');
   console.log('\n');
 }
+
+function maybeLogHelpOrVersion() {
+  if (!process.argv[2]) {
+    return;
+  }
+  if (process.argv[2].match(/(--help|-h)/)) {
+    logHelp();
+  }
+  if (process.argv[2].match(/(--version|-v)/)) {
+    console.log('v'+require('./package').version);
+  }
+  process.exit();
+}
+
+function validateArgs() {
+  if (process.argv.length !== 4) {
+    logHelp();
+    process.exit();
+  }
+}
+
